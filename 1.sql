e09b8e739ed500f7278ed8f0c8c090c44c6164a0
Destreza <Tong.len004@gmail.com>
2025-09-24 19:21:46 -0700

    Updated
    Added numbers to go with each question

--- 1.sql
+++ 1.sql
@@ -2,19 +2,19 @@
 SELECT "name", "city" FROM 'schools' WHERE "type" = "Public School District" AND NOT "type" = "Charter District";
 --  2.find the names of districts that are no longer operational.
 SELECT "name" FROM "districts" WHERE "name" LIKE '%(non-op)';
---2. how much money, on average, districts spent per-pupil last year. 
+--3. how much money, on average, districts spent per-pupil last year. 
 SELECT AVG("per_pupil_expenditure") FROM "expenditures";
---  Names and Number of schools from the 10 cities with the most public schools. Greatest to least or Alphabetcal
+-- 4. Names and Number of schools from the 10 cities with the most public schools. Greatest to least or Alphabetcal
 SELECT "city", COUNT("types") AS "Number of Schools" FROM "schools" LIMIT 10;
 
 SELECT "city", COUNT(*) FROM "schools" WHERE "type" = "Public School" GROUP BY "city" ORDER BY COUNT(*) DESC LIMIT 10;
--- write a SQL query to find cities with 3 or fewer public schools by NAME and #
+-- 5. write a SQL query to find cities with 3 or fewer public schools by NAME and #
 SELECT "city", COUNT(*) FROM "schools" WHERE "type" = "Public School" GROUP BY "city" HAVING COUNT(*) < 3 ORDER BY COUNT(*) DESC;
--- write a SQL query to find the names of schools (public or charter!) that reported a 100% graduation rate.
+-- 6. write a SQL query to find the names of schools (public or charter!) that reported a 100% graduation rate.
 SELECT "name" FROM "schools" WHERE "id" IN (
     SELECT "school_id" FROM "graduation_rates" WHERE "graduated" = 100
 );
--- names of schools (public or charter!) in the Cambridge school district. district whose name is “Cambridge.” 
+-- 7. names of schools (public or charter!) in the Cambridge school district. district whose name is “Cambridge.” 
 SELECT "name" FROM "districts" WHERE "name" LIKE '%Cambridge%';
 
 --8.display the names of all school districts and the number of pupils enrolled in each ASC School name (CHECKED UNTIL HERE)
