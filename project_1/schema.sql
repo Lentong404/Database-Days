40c8e39f5d9ca85db0036c789ecb6f7a8ce71e82
Destreza <Tong.len004@gmail.com>
2025-10-08 22:39:39 -0700

    Updated

--- project_1/schema.sql
+++ project_1/schema.sql
@@ -22,15 +22,18 @@ CREATE TABLE rentals {
     "library_id", 
     "user_id", 
     "game_id", 
-    "purchase_type" TEXT NOT NULL CHECK("purchase_type" IN('Purchase','Rental'))
+    "purchase_type" TEXT NOT NULL CHECK("purchase_type" IN('Purchase','Rental')),
     --only required if the purchase_type is "Rental"
-    "rental_date" TEXT CHECK("purchase_type" = 'Rental' AND "rental_date" IS NOT NULL)
+    "rental_date" TEXT,
     -- the date/time the user started playing the game (NULL if not started)
-    "launch_date" TEXT DATETIME(CURRENT_TIMESTAMP) NULL 
+    "launch_date" TEXT DATETIME(CURRENT_TIMESTAMP) DEFAULT NULL,
     -- manually enter as 30 days after rental_date if rented*
-    "expiry_date" TEXT CHECK ("rental_date" IS NOT NULL (DATETIME("rental_date", '+30 days')))
-    "status" TEXT NOT NULL CHECK("status" IN("Active", "Expired"))
-    PRIMARY KEY ("library_id")
-    FOREIGN KEY ("user_id") REFERENCES "users"("user_id")
-    FOREIGN KEY ("game_id") REFERENCES "games"("game_id")
+    -- What does expiry_date contain if rental_date IS Null 
+    "expiry_date" TEXT DEFAULT DATETIME("rental_date", '+30 days'),
+    "status" TEXT NOT NULL CHECK("status" IN("Active", "Expired")),
+    PRIMARY KEY ("library_id"),
+    FOREIGN KEY ("user_id") REFERENCES "users"("user_id"),
+    FOREIGN KEY ("game_id") REFERENCES "games"("game_id"),
+    -- Table level constraint to check multiple columns 
+    CHECK("purchase_type" = 'Purchase' OR ("purchase_type" = 'Rental' AND "rental_date" IS NOT NULL))
 }
