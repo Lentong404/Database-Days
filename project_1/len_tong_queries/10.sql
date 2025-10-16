2c45d783c39501e586ae4e71091fb2bee78da706
Destreza <Tong.len004@gmail.com>
2025-10-15 18:51:10 -0700

    Update 10.sql

--- project_1/len_tong_queries/10.sql
+++ project_1/len_tong_queries/10.sql
@@ -1,4 +1,4 @@
 --List all users who rented a game that expired before they started playing.
 SELECT "users"."name" FROM "users"
 JOIN "library" ON "library"."user_id" = "users"."user_id"
-WHERE "expiry_date" < "launch_date" = 'NULL' AND "status" = 'Expired'
+WHERE "expiry_date" < "launch_date" AND "status" = 'Expired'
